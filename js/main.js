78cc68b17c221a9c826ac21f7f5cd5b2ff6138db
chirpmonster <598033121@qq.com>
2020-04-18 22:24:37 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var Vue1=new Vue({
+    el:'#career',
+    data:{
+        imgs:["img/zhiye1.png",
+            "img/zhiye2.png",
+            "img/zhiye3.png",
+            "img/zhiye4.png",
+            "img/zhiye5.png",
+            "img/zhiye6.png",
+            "img/zhiye7.png",
+            "img/zhiye8.png",
+            "img/zhiye9.png",]
+    },
+    methods:{
+
+    }
+});
